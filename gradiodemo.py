b463801c10a1652b8ef57b1b0f0b2b4c5fcb8e39
hysts <hysts@users.noreply.github.com>
2021-11-15 06:36:15 +0900

    Apply pre-commit

--- gradiodemo.py
+++ gradiodemo.py
@@ -1,30 +1,28 @@
-#!pip install openmim
-#!mim install mmcv-full
-#!mim install mmdet
-#!mim install mmpose
-#!pip install anime-face-detector
-#!pip install gradio
-
 import cv2
+import gradio as gr
 import numpy as np
+import torch
+from PIL import Image
+
 import anime_face_detector
+
 #100% 235M/235M [00:01<00:00, 130MB/s]
 #100% 37.5M/37.5M [00:00<00:00, 120MB/s]
 
-import gradio as gr
-from PIL import Image
-import torch
-
-torch.hub.download_url_to_file('https://raw.githubusercontent.com/hysts/anime-face-detector/main/assets/input.jpg', 'input.jpg')
+torch.hub.download_url_to_file(
+    'https://raw.githubusercontent.com/hysts/anime-face-detector/main/assets/input.jpg',
+    'input.jpg')
 
 detector = anime_face_detector.create_detector('yolov3')
 
 FACE_SCORE_THRESH = 0.5
 LANDMARK_SCORE_THRESH = 0.3
+
+
 def hystsanimedet(img, aus_resize=512):
     image = cv2.imread(img.name)
     preds = detector(image)
-    
+
     res = image.copy()
     for pred in preds:
         box = pred['bbox']
@@ -46,24 +44,22 @@ def hystsanimedet(img, aus_resize=512):
             pt = np.round(pt).astype(int)
             cv2.circle(res, tuple(pt), lt, color, cv2.FILLED)
     res = cv2.cvtColor(res, cv2.COLOR_BGR2RGB)
-    
+
     image_pil = Image.fromarray(res)
     #image_pil = image_pil.resize(aus_resize, Image.BICUBIC)
     return image_pil
-  
-title = "hysts/anime-face-detector"
-description = "demo for hysts/anime-face-detector. To use it, simply upload your image, or click one of the examples to load them. Read more at the links below."
+
+
+title = 'hysts/anime-face-detector'
+description = 'demo for hysts/anime-face-detector. To use it, simply upload your image, or click one of the examples to load them. Read more at the links below.'
 article = "<a href='https://github.com/hysts/anime-face-detector'>Github Repo</a>"
 
-gr.Interface(
-    hystsanimedet, 
-    [gr.inputs.Image(type="file", label="Input")], 
-    gr.outputs.Image(type="pil", label="Output"),
-    title=title,
-    description=description,
-    article=article,
-    examples=[
-        ["input.jpg"],
-    ],
-    enable_queue=True
-    ).launch(debug=True,share=True)
+gr.Interface(hystsanimedet, [gr.inputs.Image(type='file', label='Input')],
+             gr.outputs.Image(type='pil', label='Output'),
+             title=title,
+             description=description,
+             article=article,
+             examples=[
+                 ['input.jpg'],
+             ],
+             enable_queue=True).launch(debug=True, share=True)
